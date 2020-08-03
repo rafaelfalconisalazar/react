02fd36804da73d3e5b69b2c81bcee92c53262438
rafael <r.falconi9030@gmail.com>
2020-08-03 17:53:27 -0500

    +

--- components/screens/TabBScreen.js
+++ components/screens/TabBScreen.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button, FlatList } from 'react-native';
+import { View, Text, Button, FlatList, ActivityIndicator } from 'react-native';
 import { ListItem } from 'react-native-elements'
 import firebase from "../firebase/firebase";
 import "firebase/firestore";
 export default function TabBscreen({ navigation }) {
     const citiesRef = firebase.firestore().collection('ciudades');
     const products = firebase.firestore().collection('products');
-
     const [data, setData] = useState([]);
+    const [one, setOne] = useState();
+    const [isLoading, setLoading] = useState(true);
     const synch = () => {
         let iphone = products.doc().set({
             description: "motorolo moto g6 plus", name: "motorola", price: 250.55
@@ -37,50 +38,52 @@ export default function TabBscreen({ navigation }) {
                 console.log('Error getting documents', err);
             });
         firebase.firestore().collection('products').doc('1').get()
-            .then(snapshot =>   console.log(snapshot.data()))
+            .then(snapshot => {
+                setOne(snapshot.data())
+            })
             .catch((err) => {
                 console.log('Error getting documents', err);
-            });
+            }).finally(() => setLoading(false));
     }
     useEffect(() => {
         let isMounted = true;
         synch();
         oneDocument();
+
         return () => { isMounted = false };
     }, [])
 
 
     return (
-        <View>
-            <Text style={{ textAlign: 'center', color: 'blue', marginTop: 300 }}>
-                Welcome to Tab B screen !
-            </Text>
-            <Button
-                onPress={() => navigation.navigate('Tab', {}, navigation.navigate('TabA Details',
-                    {
-                        papitas: 'from papitas'
-                    }))}
-                title="Learn More"
-            />
-            {
-                data.map((item, i) => (
-                    <ListItem
-                        key={i}
-                        title={item.name}
-                        leftIcon={{ name: 'av-timer' }}
-                        bottomDivider
-                        chevron
-                    />
-                ))
-            }
-            <FlatList
-                data={data}
-                keyExtractor={item => item.id}
-                renderItem={({ item }) => (
-                    <Text>{item.description}, {item.name}</Text>
-                )}
-            />
 
+        <View>
+            {isLoading ? (<ActivityIndicator />)
+                : (
+                    <View>
+                        <Text style={{ textAlign: 'center', color: 'blue', marginTop: 300 }}>
+                            Welcome to Tab B screen ! {one.name}
+                        </Text>
+                        <Button
+                            onPress={() => navigation.navigate('Tab', {}, navigation.navigate('TabA Details',
+                                {
+                                    papitas: 'from papitas'
+                                }))}
+                            title="Learn More"
+                        />
+                        {
+                            data.map((item, i) => (
+                                <ListItem
+                                    key={i}
+                                    title={item.name}
+                                    leftIcon={{ name: 'av-timer' }}
+                                    bottomDivider
+                                    chevron
+                                />
+                            ))
+                        }
+                    </View>
+                )
+            }
 
         </View>
     );
